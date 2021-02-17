f9d853e05094bced615181e9d242de1148c359cf
Diego Azevedo <dapptn@hotmail.com>
2021-02-17 11:57:10 +1100

    feat: form validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 const $removeButtons = document.querySelectorAll(".icon-button.remove");
 const $addButtons = document.querySelectorAll(".icon-button.add");
+const $form = document.querySelector("form");
+const $formFields = document.querySelectorAll(
+  "form input[type=text], form input[type=number], form select"
+);
+
+const $messageError = document.querySelector(".error-text");
+const $messageSuccess = document.querySelector(".success-text");
 
 document.addEventListener("DOMContentLoaded", () => {
   $removeButtons.forEach((element) => {
@@ -8,6 +15,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   $addButtons.forEach((element) => {
     element.addEventListener("click", increaseQuantity);
+  });
+
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    var isInputEmpty = false;
+
+    $formFields.forEach((element) => {
+      if (element.value === "") {
+        isInputEmpty = true;
+        element.parentElement.classList.add("error");
+        $messageError.classList.add("show");
+      } else {
+        element.parentElement.classList.remove("error");
+      }
+    });
+
+    if (isInputEmpty) {
+      return;
+    }
+
+    removeErrorFromInputs();
+    $messageError.classList.remove("show");
+    $messageSuccess.classList.toggle("show");
   });
 });
 
@@ -24,4 +54,11 @@ const decreaseQuantity = (event) => {
 const increaseQuantity = (event) => {
   const inputQuantity = event.target.previousElementSibling;
   inputQuantity.value = ++inputQuantity.value;
+};
+
+const removeErrorFromInputs = () => {
+  const $errors = document.querySelectorAll(".error");
+  $errors.forEach((element) => {
+    error.classList.toggle("error");
+  });
 };
